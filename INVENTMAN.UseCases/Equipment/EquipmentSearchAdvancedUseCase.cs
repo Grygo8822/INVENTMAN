4b6d703bd234e01b158cc770de07f3f7aabe3975
Grygo8822 <m.grygorowicz01@gmail.com>
2023-05-27 02:46:17 +0200

    Zmieniono język interfejsu na Polski.

---
+++ INVENTMAN.UseCases/Equipment/EquipmentSearchAdvancedUseCase.cs
@@ -0,0 +1,28 @@
+﻿using INVENTMAN.Entities;
+using INVENTMAN.UseCases.Equipment.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace INVENTMAN.UseCases.Equipment
+{
+    public class EquipmentSearchAdvancedUseCase
+    {
+        private readonly IInventoryRepository ivnetnoryRepository;
+
+        public EquipmentSearchAdvancedUseCase(IInventoryRepository ivnetnoryRepository)
+        {
+            this.ivnetnoryRepository=ivnetnoryRepository;
+        }
+
+        public async Task<IEnumerable<Item>> ExecuteAsync(string? name, EquipmentState? state, 
+            EquipmentType? type, string? serialNumber, 
+            string? employeeName,  string? invoicId, 
+            Vendor? vendor, Manufacturer? manufacturer)
+        {
+
+        }
+    }
+}
